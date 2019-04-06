f827512b1529fefe93873c47223b717fd34461e3
CedarChennn <a1249812431@gmail.com>
2019-04-06 13:34:10 +0800

    Add project files.

---
+++ myC/test01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	const float pi = 3.1415920;
+	int a = 0;
+	int len = sizeof(pi);
+	printf("hello,world\n%f",pi);
+	printf("%d", len);
+
+	return 0;
+}
