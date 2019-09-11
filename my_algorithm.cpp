ee604b96ca4f428580879cf78fa1085788c0f37d
cedarchennn <a1249812431@gmail.com>
2019-09-11 16:54:54 +0800

    add leetcode739

--- my_algorithm.cpp
+++ my_algorithm.cpp
@@ -34,6 +34,6 @@ void quick_sort(int arr[], int low, int high)
 	int temp = arr[low];
 	arr[low] = arr[j];
 	arr[j] = temp;
-	Qsort(arr, low, j - 1);
-	Qsort(arr, j + 1, high);
+	quick_sort(arr, low, j - 1);
+	quick_sort(arr, j + 1, high);
 }
